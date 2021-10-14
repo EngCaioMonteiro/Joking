ec5398bf3064eb651a6c6d1162ca588344661f30
Caio Oliveira Monteiro <caio.monteiro@grupoicts.com.br>
2021-10-14 11:48:46 -0400

    Atualização para que os dados fiquem visiveis na tela

--- Formulario 01/assets/javascript/main.js
+++ Formulario 01/assets/javascript/main.js
@@ -1,8 +1,10 @@
 const mostraDados = () => {
 
-    var nome = document.getElementById("user").value;    
-    var senha = document.getElementById("password").value;  
-    
+    var nome = document.getElementById("user").value;
+    var senha = document.getElementById("password").value;
+
+    /* document.getElementById("pegadinha").innerHTML = "O usuário é " + nome + " e a senha é " + senha; */
+
     var mostra = document.getElementById("pegadinha");
     mostra.innerHTML = "O usuário é " + nome + " e a senha é " + senha;
 
