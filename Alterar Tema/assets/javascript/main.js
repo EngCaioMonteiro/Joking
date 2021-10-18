bfdc9438db9cb563be048097096ee1a1de783b29
Caio Oliveira Monteiro <caio.monteiro@grupoicts.com.br>
2021-10-18 16:48:28 -0400

    Trabalhando a alteração de temas. Pagina HTML + JavaScript para alterer o CSS

--- Alterar Tema/assets/javascript/main.js
+++ Alterar Tema/assets/javascript/main.js
@@ -1,6 +1,33 @@
-function mudarTema() {
-    alert("Mudei o Tema");
+const botao = document.querySelector("button");
+const header = document.getElementsByTagName("header")[0];
+const footer = document.getElementsByTagName("footer")[0];
+const main = document.getElementsByTagName("main")[0];
+const h1 = document.getElementsByTagName("h1")[0];
+const dark = "Dark Mode";
+const light = "Light Mode";
+const darkModeClass = "dark-mode"
+
+botao.addEventListener("click", mudarTema);
+
+function mudarTema () {
+    mudarClasse();
+    mudarTexto();
 }
 
-var botao = document.querySelector("button")
-botao.addEventListener("click", mudarTema);
+function mudarTexto () {
+    if (botao.classList.contains(darkModeClass)) {
+        h1.innerHTML = dark + " ON"; 
+        botao.innerHTML = light + " theme";
+        return;
+    }
+    h1.innerHTML = light + " ON"; 
+        botao.innerHTML = dark + " Theme";   
+}
+
+function mudarClasse () {
+    botao.classList.toggle(darkModeClass);
+    header.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+    main.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+}
